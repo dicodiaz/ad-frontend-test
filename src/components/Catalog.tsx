c037226dbf8e224310f39777af8f1d1327e7326c
Dico Diaz Dussan <dicodiaz@gmail.com>
2025-07-13 15:15:46 -0500

    fix: catalog inline spacing

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -13,8 +13,8 @@ export const Catalog: React.FC<CatalogProps> = async ({ genre, currentPage }) =>
   });
 
   return (
-    <section className="flex flex-col gap-y-8 py-8 md:gap-y-10 md:py-10 xl:gap-y-12 xl:py-12">
-      <div className="flex flex-col gap-y-6 px-6 md:-mx-5 md:flex-row md:flex-wrap md:gap-y-9 md:px-20 xl:-mx-6 xl:gap-y-12 xl:px-32">
+    <section className="flex flex-col gap-y-8 px-6 py-8 md:gap-y-10 md:px-20 md:py-10 xl:gap-y-12 xl:px-32 xl:py-12">
+      <div className="flex flex-col gap-y-6 md:-mx-5 md:flex-row md:flex-wrap md:gap-y-9 xl:-mx-6 xl:gap-y-12">
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
--- src/components/CatalogFallback.tsx
+++ src/components/CatalogFallback.tsx
@@ -2,7 +2,7 @@ import { ImSpinner2 } from 'react-icons/im';
 
 export const CatalogFallback: React.FC = () => {
   return (
-    <div className="flex justify-center px-6">
+    <div className="flex justify-center py-8 md:py-10 xl:py-12">
       <ImSpinner2 className="size-6 animate-spin" />
     </div>
   );
