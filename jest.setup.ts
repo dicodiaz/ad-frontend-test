f124f087e560d90610e00b7b1619d0856a287940
Dico Diaz Dussan <dicodiaz@gmail.com>
2025-07-14 13:20:19 -0500

    test: fix and add tests for GenreSelect

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom';
+
+(globalThis as any).mockSearchParams = new URLSearchParams();
+(globalThis as any).mockPush = jest.fn();
+
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({
+    push: (globalThis as any).mockPush,
+  }),
+  useSearchParams: () => (globalThis as any).mockSearchParams,
+}));
--- src/__tests__/home/GenreSelect.test.tsx
+++ src/__tests__/home/GenreSelect.test.tsx
@@ -1,11 +1,41 @@
 import { GenreSelect } from '@/components';
 import { availableFilters } from '@/utils/endpoint';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
-// TODO: Fixme
 describe('GenreSelect', () => {
+  beforeEach(() => {
+    (globalThis as any).mockPush.mockClear();
+    (globalThis as any).mockSearchParams = new URLSearchParams(); // reset
+  });
+
   it('renders GenreSelect unchanged', () => {
     const { container } = render(<GenreSelect availableFilters={availableFilters} />);
     expect(container).toMatchSnapshot();
+  });
+
+  it('defaults to "all" if no genre in searchParams', () => {
+    render(<GenreSelect availableFilters={availableFilters} />);
+    expect(screen.getByRole('combobox')).toHaveValue('all');
+  });
+
+  it('shows selected genre if genre is in searchParams', () => {
+    (globalThis as any).mockSearchParams = new URLSearchParams('genre=Action');
+    render(<GenreSelect availableFilters={availableFilters} />);
+    expect(screen.getByRole('combobox')).toHaveValue('Action');
+  });
+
+  it('updates URL when genre changes', () => {
+    render(<GenreSelect availableFilters={availableFilters} />);
+    fireEvent.change(screen.getByRole('combobox'), { target: { value: 'Horror' } });
+
+    expect((globalThis as any).mockPush).toHaveBeenCalledWith('?genre=Horror');
+  });
+
+  it('removes genre param if "all" is selected', () => {
+    (globalThis as any).mockSearchParams = new URLSearchParams('genre=Comedy');
+    render(<GenreSelect availableFilters={availableFilters} />);
+    fireEvent.change(screen.getByRole('combobox'), { target: { value: 'all' } });
+
+    expect((globalThis as any).mockPush).toHaveBeenCalledWith('?');
   });
 });
